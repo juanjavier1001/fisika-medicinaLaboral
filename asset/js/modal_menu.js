daf3e1d113006481f590b215db9c80f408b35d6c
juanjavier1001 <javierdeveloper1001@gmail.com>
2025-05-15 10:34:24 -0300

    agrego menu medicina laboral caja

--- asset/js/modal_menu.js
+++ asset/js/modal_menu.js
@@ -4,3 +4,20 @@ function abrirModal() {
     const modal = new bootstrap.Modal(document.getElementById('modalCards'));
     modal.show();
 }
+
+
+const sidebar = document.getElementById('sidebarLeft');
+
+sidebar.addEventListener('hidden.bs.offcanvas', function () {
+    const collapses = sidebar.querySelectorAll('.accordion-collapse');
+
+    collapses.forEach(collapse => {
+        const bsCollapse = bootstrap.Collapse.getInstance(collapse);
+        if (bsCollapse) {
+            bsCollapse.hide();
+        } else {
+            new bootstrap.Collapse(collapse, { toggle: false }).hide();
+        }
+    });
+});
+
